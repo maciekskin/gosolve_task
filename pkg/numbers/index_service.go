d2cb209000a2bc8062991620702c3cc284f3bafc
Maciej Skindzier <maciekskin@gmail.com>
2024-10-24 21:01:51 +0200

    Change search algorithm to more efficient one

--- pkg/numbers/index_service.go
+++ pkg/numbers/index_service.go
@@ -52,22 +52,27 @@ func NewNumbersSliceRepository(numbers []int, conformationLevel int, logger *zap
 }
 
 func (s *NumbersSliceRepository) GetIndex(value int) (Number, error) {
-	for idx, number := range s.numbers {
-		if value == number {
-			s.logger.Debug("found index for value's exact match", zap.Int("value", value), zap.Int("index", idx))
-			return Number{Index: idx, Value: number}, nil
-		}
+	leftIdx := -1
+	rightIdx := len(s.numbers)
 
-		numberDiff := math.Abs(float64(number - value))
-		numberFoundInConformationLevel := numberDiff < float64(number)/float64(s.conformationLevel)
-		if numberFoundInConformationLevel {
-			// TODO:
-			// currently there is a bug that for big numbers we're not finding exact match because we find a value that fits the conformation level
-			// use binary search to have better lookup performance and to return correct indexes!
-			s.logger.Debug("found index in conformation level", zap.Int("value", value), zap.Int("index", idx))
-			return Number{Index: idx, Value: number}, nil
+	for rightIdx > leftIdx+1 {
+		middleIdx := (leftIdx + rightIdx) / 2
+		if s.numbers[middleIdx] < value {
+			leftIdx = middleIdx
+		} else {
+			rightIdx = middleIdx
 		}
 	}
 
+	// TODO: add more test cases to see if these expressions could be reduced or optimized for better match in conformation level
+	if rightIdx < len(s.numbers) && s.numbers[rightIdx] == value {
+		return Number{Index: rightIdx, Value: value}, nil
+	} else if leftIdx > -1 && s.numbers[leftIdx] == value {
+		return Number{Index: leftIdx, Value: value}, nil
+	} else if leftIdx > -1 && math.Abs(float64(s.numbers[leftIdx]-value)) < float64(s.numbers[leftIdx])/float64(s.conformationLevel) {
+		return Number{Index: leftIdx, Value: s.numbers[leftIdx]}, nil
+	} else if rightIdx < len(s.numbers) && math.Abs(float64(s.numbers[rightIdx]-value)) < float64(s.numbers[rightIdx])/float64(s.conformationLevel) {
+		return Number{Index: rightIdx, Value: s.numbers[rightIdx]}, nil
+	}
 	return Number{Index: -1, Value: value}, ErrNotFound
 }
