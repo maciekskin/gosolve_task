9bceb48fb3420aec8ac147b6726ce54bcbef9223
Maciej Skindzier <maciekskin@gmail.com>
2024-10-24 14:30:30 +0200

    Describe requirements as TODOs; add unit test data in test_input.txt file

---
+++ index_service.go
@@ -0,0 +1,8 @@
+package main
+
+// TODO:
+// - implement service with single method to look for index of given value with 10% level of conformation
+// - add unit tests
+// - move service/repository to separate package
+// - create separate repository utilizing input.txt content as a db - interface
+// - return error when such index for given value not found
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {}
+
+// TODO:
+// - create http server and define a GET endpoint
+// - add go.mod
+// - use separate request/response structs and internal data model
+// - load data from input.txt to slice in a repository during app start
+// - implement lookup service as separate module (see description in recruitment_task.md)
+// - add logging
+// - add configuration file with service port and log level (Debug, Info, Error)
+// - write unit tests
+// - add README.md with service description
+// - write Makefile
+// - return result nested in nice struct that can optionally return an error
+// - save periodically and commit to show your iterational code changes in final repository
+// - upload to GitHub
